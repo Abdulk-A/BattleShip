9ab9d13f0dc657487442d764c732840ece3ee8c2
Abdullah Abdulkareem <abdullah@Abdullahs-MacBook-Air-2.local>
2024-02-27 23:00:40 -0600

    adding some of the baseline code for the board

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+const int WIDTH = 25;
+const int HEIGHT = 25;
+
+char board[WIDTH][HEIGHT];
+
+void initBoard();
+void attack(int x, int y);
+
+int main() {
+
+    return 0;
+}
+
+void initBoard(){
+    //opening txt file
+    //fill board
+}
+
+void attack(int x, int y) {
+
+}
